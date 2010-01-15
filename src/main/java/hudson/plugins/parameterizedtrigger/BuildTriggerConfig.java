303a2060d6a60618ed907f69cb8c5bc3673aa399
mindless <mindless@dev.java.net>
2010-01-15 00:06:01 +0000

    [FIXED HUDSON-5143] merge together parameters from multiple sources to avoid multiple ParametersActions on a build.

--- src/main/java/hudson/plugins/parameterizedtrigger/BuildTriggerConfig.java
+++ src/main/java/hudson/plugins/parameterizedtrigger/BuildTriggerConfig.java
@@ -11,12 +11,15 @@ import hudson.model.ParameterDefinition;
 import hudson.model.ParameterValue;
 import hudson.model.ParametersAction;
 import hudson.model.ParametersDefinitionProperty;
+import hudson.model.Run;
 import hudson.plugins.parameterizedtrigger.AbstractBuildParameters.DontTriggerException;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.ListIterator;
 
 public class BuildTriggerConfig {
 
@@ -52,48 +55,78 @@ public class BuildTriggerConfig {
 	public List<AbstractProject> getProjectList() {
 		return Items.fromNameList(projects, AbstractProject.class);
 	}
-	
-	private ParametersAction getDefaultParameters(AbstractProject<?,?> project) {
+
+	private static ParametersAction mergeParameters(ParametersAction base, ParametersAction overlay) {
+		LinkedHashMap<String,ParameterValue> params = new LinkedHashMap<String,ParameterValue>();
+		for (ParameterValue param : base.getParameters())
+			params.put(param.getName(), param);
+		for (ParameterValue param : overlay.getParameters())
+			params.put(param.getName(), param);
+		return new ParametersAction(params.values().toArray(new ParameterValue[params.size()]));
+	}
+
+	private static ParametersAction getDefaultParameters(AbstractProject<?,?> project) {
 		ParametersDefinitionProperty property = project.getProperty(ParametersDefinitionProperty.class);
 		if (property == null) {
 			return null;
 		}
 		
 		List<ParameterValue> parameters = new ArrayList<ParameterValue>();
-		for (ParameterDefinition pd: property.getParameterDefinitions()) {
-			parameters.add(pd.getDefaultParameterValue());
+		for (ParameterDefinition pd : property.getParameterDefinitions()) {
+			ParameterValue param = pd.getDefaultParameterValue();
+			if (param != null) parameters.add(param);
 		}
 		
 		return new ParametersAction(parameters);
 	}
 	
+	List<Action> getBaseActions(AbstractBuild<?,?> build, Launcher launcher, BuildListener listener)
+			throws IOException, InterruptedException, DontTriggerException {
+		List<Action> actions = new ArrayList<Action>();
+		ParametersAction params = null;
+		for (AbstractBuildParameters config : configs) {
+			Action a = config.getAction(build, launcher, listener);
+			if (a instanceof ParametersAction) {
+				params = params == null ? (ParametersAction)a
+					: mergeParameters(params, (ParametersAction)a);
+			} else if (a != null) {
+				actions.add(a);
+			}
+		}
+		if (params != null) actions.add(params);
+		return actions;
+	}
+
+        List<Action> getBuildActions(List<Action> baseActions, AbstractProject project) {
+		List<Action> actions = new ArrayList<Action>(baseActions);
+
+		ParametersAction defaultParameters = getDefaultParameters(project);
+		if (defaultParameters != null) {
+			Action a = null;
+			for (ListIterator<Action> it = actions.listIterator(); it.hasNext();)
+				if ((a = it.next()) instanceof ParametersAction) {
+					it.set(mergeParameters(defaultParameters, (ParametersAction)a));
+					break;
+                                }
+			if (!(a instanceof ParametersAction))
+				actions.add(defaultParameters);
+		}
+		return actions;
+	}
 
 	public void perform(AbstractBuild<?, ?> build, Launcher launcher,
 			BuildListener listener) throws InterruptedException, IOException {
 
 		try {
 			if (condition.isMet(build.getResult())) {
-				List<Action> actions = new ArrayList<Action>();
-				for (AbstractBuildParameters config : configs) {
-					Action a = config.getAction(build, launcher, listener);
-					if (a != null) {
-						actions.add(a);
-					}
-				}
-
+				List<Action> actions = getBaseActions(build, listener);
 				if (!actions.isEmpty()) {
 					for (AbstractProject project : getProjectList()) {
-						
-						List<Action> list = new ArrayList<Action>(actions);
-						
-						ParametersAction defaultParameters = getDefaultParameters(project);
-						if (defaultParameters != null) {
-							list.add(0, defaultParameters);
-						}
+						List<Action> list = getBuildActions(actions, project);
 						
 						project.scheduleBuild(0,
-								new Cause.UpstreamCause(build),
-								(Action[]) list.toArray(new Action[list.size()]));
+								new Cause.UpstreamCause((Run)build),
+								list.toArray(new Action[list.size()]));
 					}
 				}
 			}
