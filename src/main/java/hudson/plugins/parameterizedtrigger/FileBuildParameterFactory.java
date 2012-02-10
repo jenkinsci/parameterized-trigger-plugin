3a48269f0c8a7ba872cdbf74d93b698889dcefc9
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-02-09 16:07:29 -0800

    Minor massaging.
    i18n, and removed unused code that I don't think never needs to be
    resurrected.

--- src/main/java/hudson/plugins/parameterizedtrigger/FileBuildParameterFactory.java
+++ src/main/java/hudson/plugins/parameterizedtrigger/FileBuildParameterFactory.java
@@ -44,83 +44,12 @@ public class FileBuildParameterFactory extends AbstractBuildParameterFactory {
     public FileBuildParameterFactory(String filePattern) {
         this.filePattern = filePattern;
     }
-    
-    public class FileParameterCollector implements FilePath.FileCallable< List<String> >  {
-        
-         //protected String filePattern;
 
-         
-         public FileParameterCollector(String filePattern) {
-             //this.filePattern = filePattern;
-         }
-
-         //private void writeObject(java.io.ObjectOutputStream out) throws IOException {
-             //String foo = "Sean";
-            //.defaultWriteObject();
-            //out.writeObject(filePattern);
-        //}
-          
-       // private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
-        //    in.defaultReadObject();
-            //filePattern = (String) in.readObject();
-        //}
-
-         public List<String> invoke(File ws, VirtualChannel channel) throws IOException {
-
-             ArrayList<String> params = Lists.newArrayList();
-             return params;
-             /*
-             
-            final long nowSlave = System.currentTimeMillis();
-            FileSet fs = Util.createFileSet(new File(ws, ""), filePattern);
-            DirectoryScanner ds = fs.getDirectoryScanner();
-            String[] files = ds.getIncludedFiles();
-
-            
-
-            if (files.length == 0) {
-
-                // no test result. Most likely a configuration error or fatal problem
-                Logger.getLogger(FileBuildParameterFactory.class.getName()).log(Level.WARNING, null, "No parameter files found that match the pattern.");
-                return params;
-            }
-
-            for(int i=0; i < files.length; ++i) {
-                BufferedReader reader = null;
-
-                try {
-                    String filename = ds.getBasedir() + System.getProperty("file.separator") + files[i];
-                    Logger.getLogger(FileBuildParameterFactory.class.getName()).log(Level.INFO, null, "Reading parameter file " + filename);
-                    reader = new BufferedReader(new FileReader(filename));
-                    String text = null;
-                    StringBuffer contents = new StringBuffer();
-                    while((text = reader.readLine()) != null) {
-                        contents.append(text).append(System.getProperty("line.separator"));
-                    }
-
-                    params.add(contents.toString());
-
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                    } catch (IOException e) {
-                    e.printStackTrace();
-                    } finally {
-                    try {
-                        if (reader != null) {
-                        reader.close();
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-
-            return params;*/
-        }
-
+    public String getFilePattern() {
+        return filePattern;
     }
 
-//    @Override
+    @Override
     public List<AbstractBuildParameters> getParameters(AbstractBuild<?, ?> build, TaskListener listener) throws IOException, InterruptedException, AbstractBuildParameters.DontTriggerException {
         
         List<AbstractBuildParameters> result = Lists.newArrayList();
@@ -161,17 +90,4 @@ public class FileBuildParameterFactory extends AbstractBuildParameterFactory {
             return Messages.FileBuildParameterFactory_FileBuildParameterFactory();
         }
     }
-
-    private static final VariableResolver<String> EMPTY_STRING_VARIABLE_RESOLVER = new VariableResolver<String>() {
-
-        @Override
-        public String resolve(String name) {
-            return "";
-        }
-    };
-    
-     public String getFilePattern() {
-        return filePattern;
-    }
-   
 }
