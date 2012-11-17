d8758e442287caebd244fc5fbbf7d06089aac377
cjo9900 <cjo.johnson@gmail.com>
2012-11-17 16:15:13 +0000

    [FIXED JENKINS-15834] Allow multiple properties files
    Support the passing of multiple property files via a
    comma seperated list.
    Correct help messages and add behaviour to correctly
    skip triggering downstream if a file is missing.

--- src/main/java/hudson/plugins/parameterizedtrigger/FileBuildParameters.java
+++ src/main/java/hudson/plugins/parameterizedtrigger/FileBuildParameters.java
@@ -3,6 +3,7 @@ package hudson.plugins.parameterizedtrigger;
 import hudson.EnvVars;
 import hudson.Extension;
 import hudson.FilePath;
+import hudson.Util;
 import hudson.model.AbstractBuild;
 import hudson.model.Action;
 import hudson.model.Descriptor;
@@ -23,46 +24,68 @@ import org.kohsuke.stapler.DataBoundConstructor;
 public class FileBuildParameters extends AbstractBuildParameters {
 
 	private final String propertiesFile;
+	private final boolean failTriggerOnMissing;
 
 	@DataBoundConstructor
-	public FileBuildParameters(String propertiesFile) {
+	public FileBuildParameters(String propertiesFile, boolean failTriggerOnMissing) {
 		this.propertiesFile = propertiesFile;
+		this.failTriggerOnMissing = failTriggerOnMissing;
 	}
 
+	public FileBuildParameters(String propertiesFile) {
+		this(propertiesFile, false);
+	}
+
+	/**
+	 * This function returns the Action that should be passed to the triggered build
+	 * to not trigger the build it can throw the DontTriggerException
+	 *
+	 * @returns Action to be passed to the triggered build, can be Null if no parameters.
+	 */
 	public Action getAction(AbstractBuild<?,?> build, TaskListener listener)
-			throws IOException, InterruptedException {
+			throws IOException, InterruptedException, DontTriggerException{
 
 		EnvVars env = getEnvironment(build, listener);
 
 		String resolvedPropertiesFile = env.expand(propertiesFile);
-		FilePath f = build.getWorkspace().child(resolvedPropertiesFile);
-		if (!f.exists()) {
-			listener
-					.getLogger()
-					.println(
-							"[parameterizedtrigger] Could not trigger downstream project, as properties file "
-									+ resolvedPropertiesFile
-									+ " did not exist.");
-			return null;
-		}
 
-		String s = f.readToString();
-		s = env.expand(s);
-		Properties p = new Properties();
-		p.load(new StringInputStream(s));
-
+		String[] allFiles = Util.tokenize(resolvedPropertiesFile, ",");
 		List<ParameterValue> values = new ArrayList<ParameterValue>();
-		for (Map.Entry<Object, Object> entry : p.entrySet()) {
-			values.add(new StringParameterValue(entry.getKey().toString(),
-					entry.getValue().toString()));
-		}
 
-		return new ParametersAction(values);
+		for(String file:allFiles) {
+			FilePath f = build.getWorkspace().child(file);
+			if (!f.exists()) {
+				listener.getLogger().println("[parameterizedtrigger] Properties file "
+					+ file + " did not exist.");
+				if(getFailTriggerOnMissing() == true) {
+					listener.getLogger().println("Not triggering due to missing file");
+					throw new DontTriggerException();
+				}
+				// goto next file.
+				continue;
+			}
+
+			String s = f.readToString();
+			s = env.expand(s);
+			Properties p = new Properties();
+			p.load(new StringInputStream(s));
+
+			for (Map.Entry<Object, Object> entry : p.entrySet()) {
+				values.add(new StringParameterValue(entry.getKey().toString(),
+						entry.getValue().toString()));
+			}
+		}
+		//Values might be empty, in that case don't return anything.
+		return values.size() == 0 ? null :new ParametersAction(values);
 
 	}
 
 	public String getPropertiesFile() {
 		return propertiesFile;
+	}
+
+	public boolean getFailTriggerOnMissing() {
+		return failTriggerOnMissing;
 	}
 
 	@Extension
