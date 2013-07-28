bf2dbf4abb44b4e644a90311a5235fa709cec714
imod <domi@fortysix.ch>
2013-07-28 14:03:47 +0200

    update/delete project references in conditional buildsteps

--- src/main/java/hudson/plugins/parameterizedtrigger/Plugin.java
+++ src/main/java/hudson/plugins/parameterizedtrigger/Plugin.java
@@ -5,12 +5,18 @@ import hudson.model.Hudson;
 import hudson.model.Item;
 import hudson.model.Project;
 import hudson.model.listeners.ItemListener;
+import hudson.tasks.BuildStep;
 import hudson.util.EnumConverter;
 import java.io.IOException;
+import java.util.List;
 import java.util.ListIterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import jenkins.model.Jenkins;
+
+import org.jenkinsci.plugins.conditionalbuildstep.ConditionalBuilder;
+import org.jenkinsci.plugins.conditionalbuildstep.singlestep.SingleConditionalBuilder;
 import org.kohsuke.stapler.Stapler;
 
 public class Plugin extends hudson.Plugin {
@@ -44,6 +50,12 @@ public class Plugin extends hudson.Plugin {
                     		changed |= co.onJobRenamed(oldName, newName);
                     	}
                     }
+                    
+                    //iterate the BuildTriggers within conditional buildsteps
+                    if(isConditionalBuildStepInstalled()) {
+                        changed |= renameInConditionalBuildStep(p, oldName, newName);
+                    }
+                    
                     //if something changed, save the project
                     if (changed){
 	                   	try {
@@ -88,6 +100,12 @@ public class Plugin extends hudson.Plugin {
                             }
                         }
                     }
+                    
+                    //iterate the BuildTriggers within conditional buildsteps
+                    if(isConditionalBuildStepInstalled()) {
+                        changed |= deleteInConditionalBuildStep(p, oldName);
+                    }
+                    
                     //if something changed, save the project
                     if (changed){
 	                    try {
@@ -105,5 +123,101 @@ public class Plugin extends hudson.Plugin {
                     }
                 }
             }
+            
+            
+            private boolean isConditionalBuildStepInstalled(){
+                final hudson.Plugin plugin = Jenkins.getInstance().getPlugin("conditional-buildstep");
+                return plugin != null ? plugin.getWrapper().isActive() : false;
+            }
+            
+            /**
+             * renames the project references within all {@link TriggerBuilder}s which are wrapped by a conditional buildsteps
+             * @param p the project the check
+             * @param oldName the old project name
+             * @param newName the new project name
+             * @return whether a change has been made
+             */
+            private boolean renameInConditionalBuildStep(Project<?,?> p, String oldName, String newName) {
+                boolean changed = false;
+                List<ConditionalBuilder> cbuilders = p.getBuildersList().getAll(ConditionalBuilder.class);
+                for (ConditionalBuilder conditionalBuilder : cbuilders) {
+                    final List<BuildStep> cbs = conditionalBuilder.getConditionalbuilders();
+                    for (BuildStep buildStep : cbs) {
+                        if(buildStep instanceof TriggerBuilder) {
+                            TriggerBuilder ctb = (TriggerBuilder)buildStep;
+                            if (ctb != null) {
+                                for (BuildTriggerConfig co : ctb.getConfigs()){
+                                    changed |= co.onJobRenamed(oldName, newName);
+                                }
+                            }
+                        }
+                    }
+                }
+                
+                
+                List<SingleConditionalBuilder> scb = p.getBuildersList().getAll(SingleConditionalBuilder.class);
+                for (SingleConditionalBuilder singleConditionalBuilder : scb) {
+                    final BuildStep buildStep = singleConditionalBuilder.getBuildStep();
+                    if(buildStep instanceof TriggerBuilder) {
+                        TriggerBuilder ctb = (TriggerBuilder)buildStep;
+                        if (ctb != null) {
+                            for (BuildTriggerConfig co : ctb.getConfigs()){
+                                changed |= co.onJobRenamed(oldName, newName);
+                            }
+                        }
+                    }                        
+                }
+                return changed;
+            }
+            
+            /**
+             * removes the project references within all {@link TriggerBuilder}s which are wrapped by a conditional buildsteps
+             * @param p the project the check
+             * @param oldName the old project name
+             * @return whether a change has been made
+             */
+            private boolean deleteInConditionalBuildStep(Project<?,?> p, String oldName) {
+                boolean changed = false;
+                
+                List<ConditionalBuilder> cbuilders = p.getBuildersList().getAll(ConditionalBuilder.class);
+                for (ConditionalBuilder conditionalBuilder : cbuilders) {
+                    final List<BuildStep> cbs = conditionalBuilder.getConditionalbuilders();
+                    for (BuildStep buildStep : cbs) {
+                        if(buildStep instanceof TriggerBuilder) {
+                            TriggerBuilder tb = (TriggerBuilder)buildStep;
+                            for (ListIterator<BlockableBuildTriggerConfig> bbtc = tb.getConfigs().listIterator(); bbtc.hasNext();) {
+                                BuildTriggerConfig c = bbtc.next();
+                                if (c.onDeleted(oldName)) {
+                                    changed = true;
+                                    if (c.getProjects().length() == 0){
+                                        bbtc.remove();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                
+                
+                List<SingleConditionalBuilder> scb = p.getBuildersList().getAll(SingleConditionalBuilder.class);
+                for (SingleConditionalBuilder singleConditionalBuilder : scb) {
+                    final BuildStep buildStep = singleConditionalBuilder.getBuildStep();
+                    if(buildStep instanceof TriggerBuilder) {
+                        TriggerBuilder tb = (TriggerBuilder)buildStep;
+                        for (ListIterator<BlockableBuildTriggerConfig> bbtc = tb.getConfigs().listIterator(); bbtc.hasNext();) {
+                            BuildTriggerConfig c = bbtc.next();
+                            if (c.onDeleted(oldName)) {
+                                changed = true;
+                                if (c.getProjects().length() == 0){
+                                    bbtc.remove();
+                                }
+                            }
+                        }
+
+                    }                        
+                }
+                return changed;
+            }
+            
         }
 }
