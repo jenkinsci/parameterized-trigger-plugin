b98707322c3f6c9ae536d1ac4f9c35e4422c2003
Nicolas De Loof <nicolas.deloof@gmail.com>
2013-08-25 17:36:30 +0200

    adapt pull request #42 to API change

--- src/main/java/hudson/plugins/parameterizedtrigger/Plugin.java
+++ src/main/java/hudson/plugins/parameterizedtrigger/Plugin.java
@@ -143,7 +143,7 @@ public class Plugin extends hudson.Plugin {
                 final List<TriggerBuilder> containedBuilders = ConditionalBuildStepHelper.getContainedBuilders(p, TriggerBuilder.class);
                 for (TriggerBuilder triggerBuilder : containedBuilders) {
                     for (BuildTriggerConfig co : triggerBuilder.getConfigs()){
-                        changed |= co.onJobRenamed(oldName, newName);
+                        changed |= co.onJobRenamed(p.getParent(), oldName, newName);
                     }
                 }
                 return changed;
@@ -161,7 +161,7 @@ public class Plugin extends hudson.Plugin {
                 for (TriggerBuilder triggerBuilder : containedBuilders) {
                     for (ListIterator<BlockableBuildTriggerConfig> bbtc = triggerBuilder.getConfigs().listIterator(); bbtc.hasNext();) {
                         BuildTriggerConfig c = bbtc.next();
-                        if (c.onDeleted(oldName)) {
+                        if (c.onDeleted(p.getParent(), oldName)) {
                             changed = true;
                             if (c.getProjects().length() == 0){
                                 bbtc.remove();
