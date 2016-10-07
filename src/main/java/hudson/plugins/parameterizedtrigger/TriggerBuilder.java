708e91f9596205c9589ab811a6da28d18160fc8e
Tomas Westling <tomasw@axis.com>
2016-10-07 09:24:39 +0200

    TriggerBuilder now implements DependencyDeclarer
    A couple of other plugins rely on the fact that the
    DependencyGraph is setup correctly, e.g. Junit and
    Downstream-buildview. This change enables TriggerBuilder,
    which is invoked when downstream builds are added as a
    build step, to work in the same way as BuildTrigger,
    which is used for post build steps.
    
    Change-Id: I38750f5f11529c4fbe7effa969f5ba35b57da3b1

--- src/main/java/hudson/plugins/parameterizedtrigger/TriggerBuilder.java
+++ src/main/java/hudson/plugins/parameterizedtrigger/TriggerBuilder.java
@@ -34,6 +34,7 @@ import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.model.Action;
 import hudson.model.BuildListener;
+import hudson.model.DependencyGraph;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.BuildStepMonitor;
 import hudson.tasks.Builder;
@@ -41,6 +42,7 @@ import hudson.model.Job;
 import hudson.model.Run;
 import hudson.model.User;
 import hudson.util.IOException2;
+import jenkins.model.DependencyDeclarer;
 import org.kohsuke.accmod.Restricted;
 import org.kohsuke.stapler.DataBoundConstructor;
 
@@ -55,7 +57,7 @@ import java.util.concurrent.Future;
  *
  * @author Kohsuke Kawaguchi
  */
-public class TriggerBuilder extends Builder {
+public class TriggerBuilder extends Builder implements DependencyDeclarer {
 
     private final ArrayList<BlockableBuildTriggerConfig> configs;
 
@@ -185,6 +187,28 @@ public class TriggerBuilder extends Builder {
     @Override
     public Collection<? extends Action> getProjectActions(AbstractProject<?, ?> project) {
         return ImmutableList.of(new SubProjectsAction(project, configs));
+    }
+
+    private boolean canDeclare(AbstractProject owner) {
+        // In Hudson 1.341+ builds will be triggered via DependencyGraph
+        // Inner class added in Hudson 1.341
+        String ownerClassName = owner.getClass().getName();
+        return DependencyGraph.class.getClasses().length > 0
+                // See HUDSON-5679 -- dependency graph is also not used when triggered from a promotion
+                && !ownerClassName.equals("hudson.plugins.promoted_builds.PromotionProcess");
+    }
+
+    @Override
+    public void buildDependencyGraph(AbstractProject owner, DependencyGraph graph) {
+        // Can only add dependencies in Hudson 1.341 or higher
+        if (!canDeclare(owner)) return;
+
+        for (BuildTriggerConfig config : configs) {
+            List<AbstractProject> projectList = config.getProjectList(owner.getParent(), null);
+            for (AbstractProject project : projectList) {
+                ParameterizedDependency.add(owner, project, config, graph);
+            }
+        }
     }
 
     @Extension
